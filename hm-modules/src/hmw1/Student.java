6494afaf8c4f68cf66be3759041e011d799fa547
Yaren Menemencioğlu <75498641+yareenm@users.noreply.github.com>
2022-06-25 03:40:51 -0500

    Update Student.java

--- hm-modules/src/hmw1/Student.java
+++ hm-modules/src/hmw1/Student.java
@@ -1,10 +1,17 @@
 package hmw1;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+// Class name is Student. It has 4 attributes; related person's name, birth date, address, and gender.
+// e-mail: yareenm@outlook.com
 public class Student {
     private String name;
     private String birthDate;
     private String address;
     private String gender;
+
+    private List<Course> courseList = new ArrayList<>();
 
     public Student(String name, String birthDate, String address, String gender){
         this.name = name;
@@ -43,5 +50,28 @@ public class Student {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(birthDate, student.birthDate) && Objects.equals(address, student.address) && Objects.equals(gender, student.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthDate, address, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", address='" + address + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
     }
 }
